a3b630285738df95aa0b6574de8f4a5a88c846a9
fromseto <wanglin44@gmail.com>
2018-01-23 15:34:05 -0500

    fix mapping of report output

--- test/MaranasTools_server_test.py
+++ test/MaranasTools_server_test.py
@@ -76,66 +76,69 @@ class MaranasToolsTest(unittest.TestCase):
     def getContext(self):
         return self.__class__.ctx
 
-    # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def load_fasta_file(self, filename, obj_name, contents):
-        f = open(filename, 'w')
-        f.write(contents)
-        f.close()
-        assemblyUtil = AssemblyUtil(self.callback_url)
-        assembly_ref = assemblyUtil.save_assembly_from_fasta({'file': {'path': filename},
-                                                              'workspace_name': self.getWsName(),
-                                                              'assembly_name': obj_name
-                                                              })
-        return assembly_ref
+    # # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
+    # def load_fasta_file(self, filename, obj_name, contents):
+    #     f = open(filename, 'w')
+    #     f.write(contents)
+    #     f.close()
+    #     assemblyUtil = AssemblyUtil(self.callback_url)
+    #     assembly_ref = assemblyUtil.save_assembly_from_fasta({'file': {'path': filename},
+    #                                                           'workspace_name': self.getWsName(),
+    #                                                           'assembly_name': obj_name
+    #                                                           })
+    #     return assembly_ref
 
-    # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
-    def test_filter_contigs_ok(self):
+    # # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
+    # def test_filter_contigs_ok(self):
 
-        # First load a test FASTA file as an KBase Assembly
-        fasta_content = '>seq1 something soemthing asdf\n' \
-                        'agcttttcat\n' \
-                        '>seq2\n' \
-                        'agctt\n' \
-                        '>seq3\n' \
-                        'agcttttcatgg'
+    #     # First load a test FASTA file as an KBase Assembly
+    #     fasta_content = '>seq1 something soemthing asdf\n' \
+    #                     'agcttttcat\n' \
+    #                     '>seq2\n' \
+    #                     'agctt\n' \
+    #                     '>seq3\n' \
+    #                     'agcttttcatgg'
 
-        assembly_ref = self.load_fasta_file(os.path.join(self.scratch, 'test1.fasta'),
-                                            'TestAssembly',
-                                            fasta_content)
+    #     assembly_ref = self.load_fasta_file(os.path.join(self.scratch, 'test1.fasta'),
+    #                                         'TestAssembly',
+    #                                         fasta_content)
 
-        # Second, call your implementation
-        ret = self.getImpl().filter_contigs(self.getContext(),
-                                            {'workspace_name': self.getWsName(),
-                                             'assembly_input_ref': assembly_ref,
-                                             'min_length': 10
-                                             })
+    #     # Second, call your implementation
+    #     ret = self.getImpl().filter_contigs(self.getContext(),
+    #                                         {'workspace_name': self.getWsName(),
+    #                                          'assembly_input_ref': assembly_ref,
+    #                                          'min_length': 10
+    #                                          })
 
-        # Validate the returned data
-        self.assertEqual(ret[0]['n_initial_contigs'], 3)
-        self.assertEqual(ret[0]['n_contigs_removed'], 1)
-        self.assertEqual(ret[0]['n_contigs_remaining'], 2)
+    #     # Validate the returned data
+    #     self.assertEqual(ret[0]['n_initial_contigs'], 3)
+    #     self.assertEqual(ret[0]['n_contigs_removed'], 1)
+    #     self.assertEqual(ret[0]['n_contigs_remaining'], 2)
 
-    def test_filter_contigs_err1(self):
-        with self.assertRaises(ValueError) as errorContext:
-            self.getImpl().filter_contigs(self.getContext(),
-                                          {'workspace_name': self.getWsName(),
-                                           'assembly_input_ref': '1/fake/3',
-                                           'min_length': '-10'})
-        self.assertIn('min_length parameter cannot be negative', str(errorContext.exception))
+    # def test_filter_contigs_err1(self):
+    #     with self.assertRaises(ValueError) as errorContext:
+    #         self.getImpl().filter_contigs(self.getContext(),
+    #                                       {'workspace_name': self.getWsName(),
+    #                                        'assembly_input_ref': '1/fake/3',
+    #                                        'min_length': '-10'})
+    #     self.assertIn('min_length parameter cannot be negative', str(errorContext.exception))
 
-    def test_filter_contigs_err2(self):
-        with self.assertRaises(ValueError) as errorContext:
-            self.getImpl().filter_contigs(self.getContext(),
-                                          {'workspace_name': self.getWsName(),
-                                           'assembly_input_ref': '1/fake/3',
-                                           'min_length': 'ten'})
-        self.assertIn('Cannot parse integer from min_length parameter', str(errorContext.exception))
+    # def test_filter_contigs_err2(self):
+    #     with self.assertRaises(ValueError) as errorContext:
+    #         self.getImpl().filter_contigs(self.getContext(),
+    #                                       {'workspace_name': self.getWsName(),
+    #                                        'assembly_input_ref': '1/fake/3',
+    #                                        'min_length': 'ten'})
+    #     self.assertIn('Cannot parse integer from min_length parameter', str(errorContext.exception))
     
     def test_run_optstoic(self):
         # inputs = { ... define inputs here ... }
         inputs = {"start_compound":"C00267","target_compound":"C00033","workspace_name":self.getWsName()}
         print "test_run_optstoic"
         # expected_outputs = { ... defined expected results here ... }
-        outputs = self.getImpl().run_optstoic(self.getContext(), inputs)
+        outputs = self.getImpl().run_optstoic(self.getContext(), inputs)[0]
         # insert some assertion that outputs = expected_outputs below.
-        self.assertEqual('awesome', 'aweome')
+        # self.assertEqual('awesome', 'aweome')
+        print(outputs)
+        self.assertIn('report_name', outputs)
+        self.assertIn('report_ref', outputs)
